bf29e67d0f370abaa4648623fc4f22aab9ab37c0
Suzette <suzetteislam@gmail.com>
2019-10-13 12:20:39 -0400

    frontend functions work and backend is functioning

--- expressPassingDataLab_Frontend.js
+++ expressPassingDataLab_Frontend.js
@@ -16,13 +16,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 const displayGif = async () =>{
     const queryValue = document.querySelector('input').value;
-    const dataServer = `http://localhost:4444/gifs?search=${queryValue}`
+    const dataServer = `http://localhost:4444/gif?search=${queryValue}`
 
     await axios
             .get(dataServer)
             .then((response)=>{
                 let result = response.data
-                console.log(result)
+                displayContent(result)
             })
             .catch((error)=>{
                 console.log('Error!')
@@ -37,19 +37,21 @@ const displayImg = async () =>{
             .get(dataServer)
             .then((response)=>{
                 let result = response.data
-                console.log(result)
+                // console.log(result)
+                displayContent(result)
             })
             .catch((error)=>{
                 console.log('Error!')
             })
 }
 
-const displayContent = () =>{
-    let contentDiv = document.createElement('div');
-    let content = document.createElement('img');
-    content.src = "";
-
-    document.appendChild(contentDiv);
-    contentDiv.appendChild(content);
-
+const displayContent = (result) =>{
+    let contentDiv = document.querySelector('div');
+    for(let i=0; i< result.length;i++){
+        let content = document.createElement('img');
+        content.src = result[i];
+        contentDiv.appendChild(content);
+    
+    }
+  
 }
--- expressPassingDataLab_backend.js
+++ expressPassingDataLab_backend.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const axios = require ('axios');
+const cors = require('cors');
 const app = express();
 
 
+app.use(cors());
 
-
-app.get('/images/:search', (request,response)=>{
-    const imgAPI = `https://pixabay.com/images/search/${req}`;
-    const req = request.params
+app.get('/images', (req,res)=>{
+    const search = req.query.search
+    
+    const imgAPI = `https://pixabay.com/api/?key=13922722-10eabd9bcf103ffa8850eae69&q=${search}`;
+    
 
     axios
     .get(imgAPI)
     .then((response)=>{
+        let result = response.data
+        let arr = result.hits
 
+        let imgsArr = [];
+        for(let i of arr){
+            imgsArr.push(i.webformatURL)
+        }
+        res.send(imgsArr)
     })
     .catch((error)=>{
         console.log('error')
@@ -20,17 +30,21 @@ app.get('/images/:search', (request,response)=>{
 
 })
 
-app.get('/gif/:search/', (request,response)=>{
-    const gifAPI = `https://api.giphy.com/v1/gifs/search?api_key=oPeDxNh6j8L19KlXicskRFXA3txlJWtS&q=${req}&limit=1`;
+app.get('/gif', (req,res)=>{
+    const search = req.query.search
+    const gifAPI = `https://api.giphy.com/v1/gifs/search?api_key=oPeDxNh6j8L19KlXicskRFXA3txlJWtS&q=${search}`;
 
-    const req = request.param
-
+    
     axios
     .get(gifAPI)
     .then((response)=>{
-        console.log(response)
-        let result = response.data;
-        console.log(result)
+        let result = response.data.data;
+
+        let gifArr = [];
+        for(let i of result){
+            gifArr.push(i.images.original.url)
+        }
+        res.send(gifArr)
     })
     .catch((error)=>{
         console.log('error')
