c372165d87442875ddbd02b656e7b689c1584d99
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-10-13 11:52:47 -0400

    Got the gif search working

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    getButton().addEventListener('click', testing)
+    gifButton().addEventListener('click', createGifs)
+    picButton().addEventListener('click', createPics)
+
 })
 
-// const axios2 = require('axios');
 
-function getButton() {
+function gifButton() {
     return document.querySelector("#searchGiphy")
 }
 
-function testing() {
+function picButton() {
+    return document.querySelector("#searchPixabay")
+}
+
+function createGifs() {
     // const myURL = 'http://localhost:1337/gifs'
-    // const resp = await axios.get(myURL) 
-    fetch("http://localhost:1337/gifs")
+    // const resp = await axios.get(myURL)
+    // https://api.giphy.com/v1/gifs/search?api_key=VeFePJ4kLDnXblmrfUBPNCu23R10GO9m&q=spongebob&limit=25&offset=0&rating=G&lang=en 
+
+    let userSearch = document.querySelector("#giphyid");
+
+    // console.log(userSearch.value);
+
+    fetch(`http://localhost:1337/gifs/?search=${userSearch.value}`)
     .then((response) => {
         return response.json();
     })
@@ -23,19 +34,53 @@ function testing() {
 function appendGifs(resp) {
 
     // let myOperator = document.querySelector('#operator').value;
-
     // const myURL = 'http://localhost:1337/gifs'
     // const resp = await axios2.get(myURL)  
     
-    let giphyGifs = document.querySelector('#giphy');
+    for(let i = 0; i < resp.length; i++) {
+        let giphyGifs = document.querySelector('#giphy');
 
-    let newImg = document.createElement('img');
+        let newImg = document.createElement('img');
+    
+        newImg.src = resp[i];
+    
+        // newImg.id = 'giphyid';
+    
+        giphyGifs.appendChild(newImg);
+    }
+    // console.log("WORK", resp[10])
+}
+// resp.data[4].images.fixed_height.url
 
-    newImg.src = resp[80];
+function createPics() {
 
-    // newImg.id = 'giphyid';
+    let userSearch = document.querySelector("#giphyid");
 
-    giphyGifs.appendChild(newImg);
+    fetch(`http://localhost:1337/gifs/?search=${userSearch.value}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then(info => {
+        appendPics(info);
+    })
+}
 
-    console.log("WORK", resp[80])
+function appendPics(resp) {
+
+    // let myOperator = document.querySelector('#operator').value;
+    // const myURL = 'http://localhost:1337/gifs'
+    // const resp = await axios2.get(myURL)  
+    
+    for(let i = 0; i < resp.length; i++) {
+        let giphyGifs = document.querySelector('#giphy');
+
+        let newImg = document.createElement('img');
+    
+        newImg.src = resp[i];
+    
+        // newImg.id = 'giphyid';
+    
+        giphyGifs.appendChild(newImg);
+    }
+    // console.log("WORK", resp[10])
 }
--- labServer.js
+++ labServer.js
@@ -9,19 +9,14 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Pic/Gif Searcher!");
 })
 
-// const makeImageArray = () => {
-//     const allImages = {
-        
-//     }
-// }
-
 app.get('/gifs', async (req, res) => {
 
     res.status(200);
 
     let printedGifs = [];
-    let searchedImages = await makeGifArray();
     const query = req.query;
+    let searchedImages = await makeGifArray(query);
+    console.log(query)
 
     for(let i = 0; i < searchedImages.data.length; i++) {
         printedGifs.push(searchedImages.data[i].images.fixed_width.url)
@@ -32,15 +27,22 @@ app.get('/gifs', async (req, res) => {
 
 })
 
-// app.get('/images', (req, res) => {
+app.get('/images', async (req, res) => {
 
-//     let printedImages = [];
+    res.status(200);
 
-//     for(let i = 0; i < printedImages.data.length; i++) {
-//         printedImages.push(pulledGifs.data[i].images.fixed_height_still.url);
-//     }
-//     res.json(printedImages);
-// })
+    let printedPic = [];
+    const query = req.query;
+    let searchedImages = await makeImageArray(query);
+    console.log(query)
+
+    for(let i = 0; i < searchedImages.data.length; i++) {
+        printedPic.push(searchedImages.data[i].images.fixed_width.url)
+    }
+    // res.json(searchedImages);
+    res.json(printedPic);
+    // res.send(query);
+})
 
 app.use("*", (req,res) => {
     res.status(404);
@@ -51,9 +53,19 @@ app.listen(1337, () => {
     console.log("Running at http://localhost:1337/")
 })
 
-async function makeGifArray () {
-    const myURL = `https://api.giphy.com/v1/gifs/search?api_key=VeFePJ4kLDnXblmrfUBPNCu23R10GO9m&q=dungeons%20and%20dragons&limit=100&offset=0&rating=G&lang=en`
+
+// https://pixabay.com/api/?key=13922718-26a724f680e1f800fd942b2fa&q=yellow+flowers&image_type=photo
+
+async function makeImageArray () {
+    const myURL = `https://pixabay.com/api/?key=13922718-26a724f680e1f800fd942b2fa&q=yellow+flowers&image_type=photo`
     const resp = await axios.get(myURL) 
-    // console.log("resp", resp.data)
+    console.log("resp", resp.data)
+    return resp.data;
+}
+
+async function makeGifArray (query) {
+    const myURL = `https://api.giphy.com/v1/gifs/search?api_key=VeFePJ4kLDnXblmrfUBPNCu23R10GO9m&q=${query.search}&limit=25&offset=0&rating=G&lang=en`
+    const resp = await axios.get(myURL) 
+    console.log("resp", resp.data)
     return resp.data;
 }
