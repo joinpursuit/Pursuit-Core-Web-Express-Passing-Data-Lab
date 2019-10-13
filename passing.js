f0c0f25dd35307d344068ca6d0eaefcea36be43d
DejaLFlynn <dejaflynn@pursuit.org>
2019-10-13 13:05:56 -0400

    almost

--- passing.js
+++ passing.js
@@ -1,6 +1,9 @@
+
 addEventListener("DOMContentLoaded", () =>{
-    console.log("content loaded")
-    setupButtonListener()
+    let button1 = document.getElementById("images").value;
+    button1.addEventListener('click', fetchServerData);
+    let button2 = document.querySelector("#button2").value;
+    button2.addEventListener('click', fetchServerData2)
 })
 const setupButtonListener = () =>{
     const loadPics = document.querySelector("form")
@@ -9,15 +12,17 @@ const setupButtonListener = () =>{
 function fetchServerData(event){
     event.preventDefault();
     
-    let search = document.querySelector("#search").value;
-    let button1 = document.querySelector("#button1").value;
-    let button2 = document.querySelector("#button2").value;
-
+    // let search = document.querySelector("#search").value;
     let url =`http://localhost:5004/${search}/${button1}/${button2}`
-    fetch(url).then(response=>{
-        return response.json()
-    }).then(total=>{
-        let images = document.querySelector("#images")
-        images.innerText = `${total.answer}`
-    })
+const stillImage = loadPics().value
+if(!stillImage) { return}
+const response = await  Axios.get(`http://localhost:5004/images?seach=${search}`)
+return response.data
+
+}
+function fetchServerData2(event){
+    event.preventDefault();
+    const gifImage = loadPics().value
+    if(!gifImage){return}
+    const response = await Axios.get(`http://localhost:5004/=gif?search=${search}` )
 }
--- server.js
+++ server.js
@@ -1,25 +1,77 @@
 const express = require('express')
 const app = express();
+const axios =require('axios')
+const bodyParser = require('body-parser')
+const port = 5004;
+const cors = require('cors')
+
+app.use(cors());
+
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+let emptyObj = {};
 
-// app.get('/', (request,response)=>{
-//     response.send(query)
+app.get('/gif/:search', (request, response)=>{
+    let urls = `https://api.giphy.com/v1/gifs/search?api_key=DqbAfGDrEbDCiyGtVnUjCl2PCJDqHXVD`;
+    let load = request.query.search;
+    let gif = axios.get(urls+ "&q="+load + "&limit=1")
+    // .then(function(response){  
+    for(let i of response.data.data) {
+        emptyObj[i.id] = i.images.original.urls;
+    }
+    
+    response.json(gif.data)
+    console.log(gif.data)
+    // response.send(response.data)
+
+})
+app.get('/images/:search',(request, response)=>{
+    let urls = `https://pixabay.com/api/?key=13923830-df4661fc6f67f3ecc5b2d1c5b`;
+    let load = request.query.search;
+    let pic = axios.get(urls+ "&q="+load + "&limit=1")
+    // .then(function(response){  
+    for(let i of response.data.hits) {
+        emptyObj[i.id] = i.webformatURL;
+    }
+    
+    response.json(pic.data)
+    console.log(pic.data)
+    // response.send(response.data)
+
+})
+
+
+// app.get("/", (request,response)=>{
+//     response.send('is running')
 // })
-app.get('/dog/:breed', (request, response)=>{
-    console.log('req.params',request.body)
-response.send('/dog route hit with params: '+ JSON.stringify(request.params))
-})
-app.post('/load', (request, response)=>{
-console.log(request);
-response.send('load')
-})
+// app.get("/gif", (request, response)=>{
 
-app.get('/', (request, response)=>{
-    console.log(request.query)
-    response.send('types')
-})
-app.listen(5004, ()=>{
+// })
+
+app.listen(port, ()=>{
     console.log('running at http://localhost:5004/')
 })
+
+// const imageArray = ()=>{
+//     const images =[];
+//     for(let i=0; i<=100; i++){
+//         const image ={
+//             jpeg:"",
+//             gif: ""
+
+//         }
+//         if(i%2){
+//             image.jpeg ="still image"
+//         } else {
+//             image.gif = "gif image"
+//         }
+//         images.push(image)
+//     }
+//     return images;
+// }
+
+// app.post('/load', (request, response)=>{
+// console.log(request);
+// response.send('load')
+// })
