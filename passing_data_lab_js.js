35258c826a76e3ce4cbc579e6251f74e13eb37e2
tbriany <brianytaveras@pursuit.org>
2019-10-13 00:07:14 -0400

    html displays photos based on search entry but doesn't display gifs

--- passing_data_lab_js.js
+++ passing_data_lab_js.js
@@ -1,4 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    listenToGifBtn();
+    listenToImgBtn();
 })
 
+const listenToGifBtn = () => {
+  let gifBtn = document.querySelector("#gifBtn")
+   gifBtn.addEventListener('click', getGifs)
+}
+
+const listenToImgBtn = () => {
+    let imgBtn = document.querySelector("#imgBtn")
+    imgBtn.addEventListener('click', getImgs)
+}
+
+const getGifs = () => {
+  let userSearch = document.querySelector('#text').value 
+  let url = `http://localhost:3000/gif/?search=${userSearch}`
+  fetch(url)
+  .then(response => response.json())
+  .then(data => displayResults(data))
+}
+
+const getImgs = () => {
+    let userSearch = document.querySelector('#text').value 
+  let url = `http://localhost:3000/image/?search=${userSearch}`
+  fetch(url)
+  .then(response => response.json())
+  .then(data => displayResults(data))
+}
+
+const displayResults = (data) => {
+  console.log(data)
+  let dataArr = data 
+  
+  for (let i = 0; i < dataArr.length; i++){
+     let results = dataArr[i]
+     let newImg = document.createElement('img')
+     newImg.src = results 
+     let resultsDiv = document.querySelector("#results")
+     resultsDiv.append(newImg)
+  }
+}
