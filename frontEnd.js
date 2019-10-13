f2888f65fab9d80179678964588039d0ee5198e9
vonbarown <vonielbrown@pursuit.org>
2019-10-12 21:57:35 -0400

    adding the functionality of the buttons

--- frontEnd.js
+++ frontEnd.js
@@ -1,26 +1,57 @@
-const myURL = `http://localhost:8080/`
+let info;
 
 document.addEventListener('DOMContentLoaded', () => {
     let gifButton = document.querySelector('#gif');
     gifButton.addEventListener('click', clickGifSearch)
 
+    let imgButton = document.querySelector('#images');
+    imgButton.addEventListener('click', clickImgSearch)
+
 })
 
 
-// const clickGifSearch = async (event) => {
-//     let input = document.querySelector('input').value
+const loadDataFromServer = async (url) => {
+    const {
+        data
+    } = await axios.get(url);
 
-//     let imgUrl = `${myURL}images?search=`
-// }
+    return data
+}
 
 const clickGifSearch = async () => {
     let input = document.querySelector('input').value;
     let gifUrl = `http://localhost:8000/gif?search=${input}`;
 
-    const {
-        data
-    } = await axios.get(gifUrl)
+    info = await loadDataFromServer(gifUrl)
+    return displayToScreen(info)
+}
 
-    console.log(data);
+const clickImgSearch = async () => {
+    let input = document.querySelector('input').value;
+    let imgUrl = `http://localhost:8000/images?search=${input}`;
 
+    info = await loadDataFromServer(imgUrl)
+    return displayToScreen(info)
+}
+
+const displayToScreen = (source) => {
+    clearScreen()
+    let results = document.querySelector('#searchResults');
+
+    let img = document.createElement('img')
+    let randomNum = getRandomNum(source.length, 0)
+    console.log(randomNum);
+
+    img.src = (source[randomNum])
+
+    results.append(img)
+}
+
+const getRandomNum = (max, min) => Math.floor((Math.random() * max) + min);
+
+const clearScreen = () => {
+    let container = document.querySelector('#searchResults');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
