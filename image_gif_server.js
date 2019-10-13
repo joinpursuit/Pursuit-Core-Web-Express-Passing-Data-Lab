296d32714494fea33a8d9424b827b055aeef279a
Hupaul <hupaulcamacho@pursuit.org>
2019-10-13 14:13:42 -0400

    working gif search query results shown on html

--- image_gif_server.js
+++ image_gif_server.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser")
-let port = 3000
+const bodyParser = require("body-parser");
+let port = 3000;
+const axios = require("axios");
+const cors = require("cors");
+app.use(cors())
 
 app.use(bodyParser.urlencoded( {
     extended: false
 }))
 
-app.get("/gifs", () => {
+app.get("/gifs/:search", async (request, response) => {
+    const myUrl = "https://api.giphy.com/v1/gifs/search?api_key=kGWrS9X81HIL2z5b05i2V64xJ6TvoWr5";
+    let data = []
+    
+    let searchTerm = request.params.search
+    let axiosResponse = await axios.get(myUrl + "&q=" + searchTerm + "&limit=25");
 
+    for(let key of axiosResponse.data.data) {
+        data.push(key.images.original.url)
+    }
+
+    console.log(data)
+    response.json(data)
 })
 
-app.get("/images", () => {
-    
+app.get("/images/:search", () => {
+    let data = {}
 })
 
 app.listen(port, () => {
--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let imgButton = document.getElementById("imgButton");
-    imgButton.addEventListener('click', loadImageFromServer);
+    // let imgButton = document.getElementById("imgSearch");
+    // imgButton.addEventListener('click', loadImageFromServer);
 
-    let gifButton = document.getElementById("imgButton");
-    gifButton.addEventListener('click', loadGifFromServer);
+    let gifButton = document.getElementById("gifSearch");
+    gifButton.addEventListener('click', renderGifImages);
 })
 
-const loadImageFromServer = () => {
-    let button = document.getElementById("imgButton")
+const renderGifImages = async () => {
+    console.log("button was clicked")
+    let searchQuery = document.getElementById("search").value;
+    const myUrl = `http://localhost:3000/gifs/${searchQuery}`;
+    const response = await axios.get(myUrl);
+    let results = document.getElementById("image-results");
+
+    for(let i of response.data) {
+        let img = document.createElement("img");
+        img.src = i
+        results.append(img);
+    }
+    
 }
 
-const loadGifFromServer = () => {
-    let button = document.getElementById("gifButton")
+const loadImageData = async () => {
+    const myURL = "http://localhost:3000/"
+    const response = await axios.get
+}
+
+const renderImageFromServer = () => {
+    let button = document.getElementById("imgButton");
+    let searchQuery = document.getElementById("search");    
+}
+
+const renderGifFromServer = () => {
+    let button = document.getElementById("gifButton");
+    let searchQuery = document.getElementById("search");
+
 }
