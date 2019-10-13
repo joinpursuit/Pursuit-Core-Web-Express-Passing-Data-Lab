a82955c3ca478f9f7c041211eb5f82f6ce7c1667
KathyPuma <kathypuma96@gmail.com>
2019-10-13 14:06:00 -0400

    almost done

--- backend.js
+++ backend.js
@@ -4,43 +4,43 @@ var axios = require('axios');
 const app= express();
 
 app.use(cors());
+// app.get('/gifs', async (req,res) =>{
+    
+//     let search = req.query.search
+//     let apiRandomSearchUrl = `https://api.giphy.com/v1/gifs/search?api_key=scQk18P2oMykircXwoLXgsvGbP0uCZtz&q=${search}`
+//     let resp = await axios.get(apiRandomSearchUrl)
+// res.json(resp.data)
+// })
 
-app.get('/gifs', (req,res) =>{
-    async function apiGif() {
+
+app.get('/gifs', async (req,res) =>{
         let search = req.query.search
         let apiRandomSearchUrl = `https://api.giphy.com/v1/gifs/search?api_key=scQk18P2oMykircXwoLXgsvGbP0uCZtz&q=${search}`
         let resp = await axios.get(apiRandomSearchUrl)
-
+        let arr = []
             for(let i=0;i< resp.data.data.length;i++){
-                console.log(resp.data.data[i].url)
+                arr.push(resp.data.data[i].url)
                  }
-
-    }
-
-    res.json(apiGif())
-
-})
-
-app.get('/images', (req,res) =>{
-    async function apiImage() {
-        let search = req.query.search
-        let apiRandomSearchUrl = `https://pixabay.com/api/?key=13922629-da0bb635de15506cfc4a4d3f3&q=${search}`
-        let resp = await axios.get(apiRandomSearchUrl)
-        for(let i=0; i<resp.data.hits.length ; i++){
-            console.log(resp.data.hits[0].webformatURL)
-
-        }
-        
-
-    }
-    res.json(apiImage())
-
-
+    res.json(arr)
 
 })
 
 
-// app.post()
+
+
+app.get('/images', async(req,res) =>{
+    let search = req.query.search
+    let apiRandomSearchUrl = `https://pixabay.com/api/?key=13922629-da0bb635de15506cfc4a4d3f3&q=${search}`
+    let resp = await axios.get(apiRandomSearchUrl)
+    let arr = [];
+    for(let i=0; i<resp.data.hits.length ; i++){
+    arr.push(resp.data.hits[i].webformatURL) 
+}
+res.status(200)
+ res.set('Content-Type', 'application/json')
+res.json(arr)
+})
+
 app.listen(4000, () =>{
-    console.log("Server running at port: 4000")
+console.log("Server running at port: 4000")
 })
