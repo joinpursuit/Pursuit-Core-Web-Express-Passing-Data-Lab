f50e7c62f97b7dc037d04e49299dcd6f97d64c29
karenm09 <karenm929@hotmail.com>
2019-10-14 19:46:55 -0400

    2nd commit

--- express.js
+++ express.js
@@ -1,4 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let resBtn = document.querySelector('#res-btn')
-    resBtn.addEventListener('click', loadDataFromServer)
+document.addEventListener('DOMContentLoaded', () => {
+    getImageBtn()
+    getGifBtn()
 })
+
+
+const getImageBtn = () => {
+
+   let imageBtn= document.querySelector("#image")
+   imageBtn.addEventListener('click', loadDataFromServer)
+}
+
+const getGifBtn = () => {
+    let gifBtn = document.querySelector("#gif")
+    gifBtn.addEventListener('click', loadDataFromServer)
+}
+
+async function loadDataFromServer() {
+    let searchInput = document.querySelector('#search-value').value
+    const myURL = `http://localhost:4000/gif${searchInput}`
+    const res = await axios.get(myURL)
+    console.log(res.data)
+   // displayDataFromServer(res.data)
+}
+
