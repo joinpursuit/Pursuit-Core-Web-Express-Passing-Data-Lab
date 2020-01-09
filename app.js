9f58eb58dee45396b7892ff244085e4b48bf1b9f
Kevin Wong <kevinwong@pursuit.org>
2020-01-09 12:54:23 -0500

    install and add body-parser package

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-app.use(cors());
+const bodyParser = require('body-parser');
+
 const port = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
 
 app.listen(port, () => {
     console.log("Listening on port ", port);
