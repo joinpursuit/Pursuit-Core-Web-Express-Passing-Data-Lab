b7f8f5ff369e0d0de837ff988dad48ea1af99656
Kevin Wong <kevinwong@pursuit.org>
2020-01-09 14:46:17 -0500

    add two endpoint routes /gif and /image, work on DOM logic

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const bodyParser = require('body-parser');
 const port = 3000;
 
 const app = express();
+let container = [];
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.post("/gif", (req, res) => {
+    console.log(req.body);
+    container.push(req.body);
+    res.json({gif: req.body});
+})
 
+app.post("/image", (req, res) => {
+
+})
 
 app.listen(port, () => {
     console.log("Listening on port ", port);
--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello");
+
+    let container = document.querySelector("#container");
+    let userInput = document.querySelector("#searchBar");
+    let gifSearch = document.querySelector("#gifSearch");
+    let imgSearch = document.querySelector("#imgSearch");
+
+    gifSearch.addEventListener("click", () => {
+        axios.get(`https://api.giphy.com/v1/gifs/search?api_key=EuNi39MeeBPzJPWLqTWVYXxSFFgecZqB&q=${userInput.value}&limit=25&offset=0&rating=PG-13&lang=en`).then(res => {
+            let data = res.data.data;
+            data.forEach(gif => {
+                let img = document.createElement('img');
+                img.src = gif.url;
+                container.appendChild(img);
+            })
+        })
+    })
+
+    imgSearch.addEventListener("click", () => {
+        debugger
+    })
+
+
 })
