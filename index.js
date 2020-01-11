367c64e8b78a2aa84a8aa31a1a65f55e8c30d742
ashya <ashyamanning@pursuit.org>
2020-01-11 13:49:38 -0500

    set up frontend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let gifSearch = document.querySelector("#gifSearch");
     let imageSearch = document.querySelector("#imageSearch");
     let ul = document.querySelector("ul");
+    let search = document.querySelector("#searchResults");
 
     gifSearch.addEventListener("click", async (e) => {
         ul.innerHTML = "";
@@ -13,6 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
             img.src = el.images.downsized_large.url;
             li.appendChild(img);
             ul.appendChild(li);
+            search.appendChild(ul);
+        })
+    })
+
+    imageSearch.addEventListener("click", async (e) => {
+        ul.innerHTML = "";
+        let results = await axios.get(`http://localhost:3000/images?search=${input.value}`);
+        results.data.images.forEach((el) => {
+            let li = document.createElement("li");
+            let img = document.createElement('img');
+            img.src = el.webformatURL;
+            li.appendChild(img);
+            ul.appendChild(li);
+            search.appendChild(ul);
         })
     })
 })
