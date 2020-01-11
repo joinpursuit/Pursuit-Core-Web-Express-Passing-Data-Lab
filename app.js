e1f618939d8f553e37d401c2a9fddb7203888ece
Phillip <phil.awich@gmail.com>
2020-01-11 12:30:22 -0500

    Add packages $ git ignore

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express();
 
 port = 3000;
 
+app.get("/", (req,res) => {
+    req.url("")
+})
+
+app.get("/gifs", (req,res) => {
+    await axios.get(``)
+    res.json({gifs: [   ]})
+})
+
 app.listen(port, () => {
     console.log("App is listening on port ", port)
 })
