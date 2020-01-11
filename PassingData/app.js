ae38106d4f65ebaa7b33d3f540bbac374a3c52df
bsihame <sihamebazi@pursuit.org>
2020-01-11 12:53:16 -0500

    add minor change

--- PassingData/app.js
+++ PassingData/app.js
@@ -11,10 +11,10 @@ let info = [];
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.get("/gif", async(request,response)=>{
+app.get("/gifs", async(request,response)=>{
   try{
     let api_key = `DqbAfGDrEbDCiyGtVnUjCl2PCJDqHXVD`
-    let gifs = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key= ${api_key} ${request.query.search}&limit=&offset=0&rating=PG&lang=en`)
+    let urlGif = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key= ${api_key} ${request.query.search}&limit=&offset=0&rating=PG&lang=en`);
 
     request.json({
       success:200,
@@ -29,7 +29,8 @@ app.get("/gif", async(request,response)=>{
 app.get("/images", async(request,response)=>{
   try{
     let api_key = `14881762-213b8595c2d5d771b7010b29e`
-    let url= await axios.get(`https://pixabay.com/api/?key= ${api_key} ${request.query.search}`);
+    let urlImages = await axios.get(`https://pixabay.com/api/?key= ${api_key}&q=${request.query.search}`);
+    
     request.json({
       success:200,
     })
--- PassingData/index.js
+++ PassingData/index.js
@@ -1,9 +1,39 @@
-document.addEventListener("DOMContentLoaded",()=>{
 
+document.addEventListener("DOMContentLoaded",()=>{
+    //loadImages();
+    loadGifs();
 })
 
-const loadImages = async() =>{
-    let button = document.querySelector("imageSearch")
-    let div = document.querySelector("div")
-    let li = document.createElement("li")
+const loadGifs = async() =>{
+    let button = document.querySelector("#gifSearch");
+    let div = document.querySelector("div");
+    console.log(button)
+    button.addEventListener("click",(e) => {
+        //https://api.giphy.com/v1/gifs/search?api_key= ${api_key} ${request.query.search}&limit=&offset=0&rating=PG&lang=en
+        axios.get("http://localhost:3000/gifs/search?q=").then(response =>{
+            let ul = document.querySelector("ul");
+            response.data.forEach(image => {
+                debugger
+                let li = document.createElement("li");
+                // li.innerText = image.
+                
+            });
+        })
+    })
 }
+
+// const loadImages = async() =>{
+//     let button = document.querySelector("#imageSearch");
+//     let div = document.querySelector("div");
+//     button.addEventListener("click",(e) => {
+//         axios.get("http://localhost:3000/images/?search=").then(response =>{
+//             let ul = document.querySelector("ul");
+//             response.data.forEach(image => {
+//                 debugger
+//                 let li = document.createElement("li");
+//                 // li.innerText = image.
+                
+//             });
+//         })
+//     })
+// }
