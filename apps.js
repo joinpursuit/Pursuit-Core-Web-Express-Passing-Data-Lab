dbeba46a3b1d65dc284b79898577e043e676d110
cassidybeni <cassidybeni@pursuit.org>
2020-01-11 15:00:09 -0500

    app.get image

--- apps.js
+++ apps.js
@@ -13,17 +13,27 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get("/gif", async (req, res) => {
+    let gifArr = []
     try {
-    let gifs = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jEawtlGCmvF2hqdMR4dG5JCteM9SibcI&q=${req.query.search}&limit=25&offset=0&rating=G&lang=en`) 
-    res.json({success:200,
-    gifs: gifs.data
-})
-
-} catch (error) {
+        let gifs = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jEawtlGCmvF2hqdMR4dG5JCteM9SibcI&q=${req.query.search}&limit=25&offset=0&rating=G&lang=en`) 
+        console.log(gifs.data.data[0].images.downsized_large.url)
+        gifs.data.data.forEach((gif) => {
+            gifArr.push(gif.images.downsized_large.url)
+        })
+        res.json(gifArr)
+    }catch (error) {
     
 }
 })
 
+app.get("/images", async (req, res) => {
+    let imageArr = []
+    try {
+        let images = await axios.get(`https://pixabay.com/api/?key=14882075-baeaa3ace3ad3915c9cb9e804&q=${req.query.search}&image_type=photo`)
+    } catch (error) {
+        
+    }
+}
 
 app.listen(port, () => {
     console.log("Listening to port ", port)
