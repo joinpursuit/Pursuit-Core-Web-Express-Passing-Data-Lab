0431a838719ffb260d94a474e8eaeccf85124b15
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-11 14:06:02 -0500

    server updates

--- PassingData/app.js
+++ PassingData/app.js
@@ -13,29 +13,39 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.get("/gif", async(request,response)=>{
   try{
+    let emptyGifArr =[]
     let api_key = `DqbAfGDrEbDCiyGtVnUjCl2PCJDqHXVD`
-    let gifs = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key= ${api_key} ${request.query.search}&limit=&offset=0&rating=PG&lang=en`)
-
+    let gifs = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key= ${api_key}&q=${req.query.search}&limit=25&offset=0&rating=G&lang=en`)
+    let gifArr= gifs.data.data;
+    gifArr.forEach(el=>{
+      emptyGifArr.push(el.images)
+    })
     request.json({
+      emptyGifArr,
       success:200,
       
     })
 
   }catch(err){
-    console.log(err)
+    response.json((err))
   }
 })
 
 app.get("/images", async(request,response)=>{
   try{
+    let emptyImageArr = []
     let api_key = `14881762-213b8595c2d5d771b7010b29e`
-    let url= await axios.get(`https://pixabay.com/api/?key= ${api_key} ${request.query.search}`);
+    let url= await axios.get(`https://pixabay.com/api/?key=${api_key}&q=${req.query.search}&image_type=photo`);
+    let imageArr = url.data.hits
+    imageArr.forEach(el=>{
+      emptyImageArr.push(el.webformatURL)
+    })
     request.json({
       success:200,
     })
 
   }catch(err){
-    console.log(err)
+    response.json((err))
   }
 })
 
