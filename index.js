e9a86ab30e63143636d2ba5e8669472ecfbb60b6
maria liza <mariamartinez@pursuit.org>
2020-01-11 14:51:47 -0500

    finish lab

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ let gif = document.querySelector("#gif")
 let imageSearch = document.querySelector("#imageSearch")
 
 gif.addEventListener("click", () => {
-    let ul = document.querySelector("ul")
-    ul.innerHTML = ""
+  let ul = document.querySelector("ul")
+  ul.innerHTML = ""
   axios.get(`http://localhost:1000/gif/?search=${input.value}`).then(res => {
     //   debugger
-      res.data.gifs.data.forEach(pics=>{
-        let img = document.createElement("img")
-        img.src = pics.images.downsized.url
-        ul.appendChild(img)
+    res.data.gifs.data.forEach(pics => {
+      let img = document.createElement("img")
+      img.src = pics.images.downsized.url
+      ul.appendChild(img)
     })
   })
+})
 
-
-
+imageSearch.addEventListener("click", () => {
+  let ul = document.querySelector("ul")
+  ul.innerHTML = ""
+  axios.get(`http://localhost:1000/images/?search=${input.value}`).then(res => {
+    debugger
+    res.data.images.hits.forEach(el => {
+      let img2 = document.createElement("img")
+      img2.src = el.largeImageURL
+      ul.appendChild(img2)
+    })
+  })
 })
