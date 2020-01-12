3d7e24355ee0209cdb3a79227122c64f31cb1513
John Jones Jr <jayjohnjones@gmail.com>
2020-01-12 17:31:49 -0500

    Frontend and back end

--- function.js
+++ function.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    let input = document.querySelector("input")
+    let gifBtn = document.querySelector("#gif")
+    let imgBtn = document.querySelector("#image")
+
+   const getData = () => {
+       axios.get("http://localhost/3000").then((res) =>{
+           
+       })
+   }
 })
